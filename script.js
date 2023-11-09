2ad573dd209ebea3d153f025ca924549f3e45f1b
Yavuz7 <yavuzyurtseven1@gmail.com>
2023-11-09 17:29:13 -0500

    Set up functionality for modal

--- script.js
+++ script.js
@@ -34,6 +34,13 @@ document.addEventListener("DOMContentLoaded", () => {
   let closeButton = document.querySelector(".modal-title-button");
   let icons = document.querySelectorAll(".desktop-icon");
 
+  // Modal Info
+  let modalTitle = document.querySelector(".modal-title-text");
+  let modalMedia = document.querySelector(".modal-media");
+  let modalText = document.querySelector(".modal-banner-text");
+  let modalButton = document.querySelector(".modal-banner-button");
+  let modalButtonLink = document.querySelector(".modal-banner-button-link");
+
   closeButton.addEventListener("click", () => {
     modal.style.display = "none";
   });
@@ -45,15 +52,31 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   icons.forEach((icon) => {
-    icon.addEventListener("dblclick", populateModal);
+    icon.addEventListener("dblclick", modalHandler);
   });
 
-  function populateModal(evt) {
+  function modalHandler(evt) {
     // Get Classes Of Current Target
     classes = evt.currentTarget.classList;
     classes.forEach((className) => {
-      if (modalInfo.find(className)) {
-        console.log("Found");
+      //Go Through List to find the class that matches the id, usually the second in the list
+      if (modalInfo.find((element) => element.id == className) != null) {
+        //If the icon is a link, open the link, otherwise populate modal and display
+        let data = modalInfo.find((element) => element.id == className);
+        if (data.isLink == "true") {
+          window.open(data["linkUrl"]);
+        } else {
+          // let modalTitle = document.querySelector(".modal-title-text");
+          // let modalMedia = document.querySelector(".modal-media");
+          // let modalText = document.querySelector(".modal-banner-text");
+          // let modalButton = document.querySelector(".modal-banner-button");
+          modalTitle.textContent = data["title"];
+          modalMedia.textContent = data["media"];
+          modalText.textContent = data["text"];
+          modalButton.textContent = data["button"];
+          modalButtonLink.href = data["buttonUrl"];
+          modal.style.display = "block";
+        }
       }
     });
     //Check Which class Matches the key in the Data Array
@@ -62,7 +85,6 @@ document.addEventListener("DOMContentLoaded", () => {
     // Set Data of Modal To Those Classes
 
     //Display The Final Modal
-    modal.style.display = "block";
   }
 
   const modalInfo = [
@@ -80,6 +102,42 @@ document.addEventListener("DOMContentLoaded", () => {
       id: "aboutSite",
       isLink: "true",
       linkUrl: "https://github.com/Yavuz7/PortfolioSite/blob/main/README.md",
+    },
+    {
+      id: "ufcFightSim",
+      isLink: "false",
+      title: "UFC Fight Simulator Project",
+      media: "Display Video Here",
+      text: "This is a group project I did in the senior year of my class!",
+      button: "Github",
+      buttonUrl: "https://github.com/Yavuz7/UFCFightSim",
+    },
+    {
+      id: "aspireHigh",
+      isLink: "false",
+      title: "Aspire High Youth Development Internship",
+      media: "Display Video Here",
+      text: "This is the internship I'm currently working. The Website I've been working on isn't up yet.",
+      button: "Website",
+      buttonUrl: "https://aspirehighyouth.org/",
+    },
+    {
+      id: "dotr",
+      isLink: "false",
+      title: "Yugioh DOTR Remake (Demo)",
+      media: "Display Video Here",
+      text: "This is a group project I did for my 3D Game Development class!",
+      button: "Github",
+      buttonUrl: "https://github.com/Yavuz7/VulkanProjectGF3D",
+    },
+    {
+      id: "quake2",
+      isLink: "false",
+      title: "Quake 2 Boss Mod",
+      media: "Display Video Here",
+      text: "This is a project I did for my game mod class!",
+      button: "Github",
+      buttonUrl: "https://github.com/Yavuz7/UFCFightSim",
     },
   ];
 });
